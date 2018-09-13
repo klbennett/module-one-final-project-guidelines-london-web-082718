6dc178cc58937c7b507e1d89ddc8159092485827
klbennett <https://github.com/settings/applications>
2018-09-13 15:24:07 +0100

    Semi-working version

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
-# frozen_string_literal: true
-source "https://rubygems.org"
-
-gem "sinatra-activerecord"
-gem "sqlite3"
-gem "pry"
+#source "https://rubygems.org"
+gem 'pry'
+gem "rspec"
 gem "require_all"
+gem "sinatra-activerecord"
+gem "activerecord"
+gem "sqlite3"
+gem "rest-client"
+gem "rake"
+gem "database_cleaner"
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,36 @@
-require_relative '../config/environment'
+# require 'active_record'
+require_relative '../config/environment.rb'
+require_relative "../lib/command_line_interface.rb"
 
+system('clear')
+welcome
+puts "Are you a returning user?(y or n)"
+y_or_n = gets.chomp
+system('clear')
+case y_or_n
 
-
-puts "HELLO WORLD"
+  when "y"
+  puts "What is your name?"
+  ans_name = gets.chomp
+    if @user = User.find_by(name: ans_name)
+        system('clear')
+        puts "Welcome back #{@user.name}"
+        run_app
+    else puts "Name not found."
+        @user = create_new_user
+          puts "Welcome #{@user.name}"
+        run_app
+    end
+  when "n"
+  # puts "What is your name?"
+  @user = create_new_user
+  system('clear')
+  puts "Welcome #{@user.name}"
+  run_app
+else
+  puts "Input not found"
+  # puts "What is your name?"
+  @user = create_new_user
+  system('clear')
+  puts "Welcome #{@user.name}"
+end
